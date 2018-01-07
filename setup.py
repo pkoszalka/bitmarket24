801178faedd40e86bf3880688aa466360bac941f
Pawel Koszalka <pawel.koszalka@yahoo.pl>
2018-01-07 17:04:44 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='bitmarket24',
+    author='pkoszalka',
+    author_email='pawel.koszalka@yahoo.pl',
+    url='https://github.com/pkoszalka/bitmarket24',
+    version='0.6',
+    packages=['bitmarket24', ],
+    description='API client for Bitmarket24 cryptocurrency trading platform.',
+    license='MIT License',
+    install_requires=[
+        'PyJWT',
+        'requests',
+    ],
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ]
+)
